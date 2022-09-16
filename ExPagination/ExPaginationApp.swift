f1e3584f5bd37cf4fb48103464d50bf1327dad93
jake <palatable7@naver.com>
2022-09-17 01:32:40 +0900

    Initial Commit

---
+++ ExPagination/ExPaginationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExPaginationApp.swift
+//  ExPagination
+//
+//  Created by 김종권 on 2022/09/17.
+//
+
+import SwiftUI
+
+@main
+struct ExPaginationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
